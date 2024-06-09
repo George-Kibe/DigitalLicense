d69de8b1477c9d8fc2e2c89ea607427aa4aa0c5d
George-Kibe <georgekibewambui@gmail.com>
2024-06-09 17:44:46 +0300

    adding amplify auth

--- App.js
+++ App.js
@@ -1,13 +1,42 @@
 import React from 'react';
+import outputs from './amplify_outputs.json';
+import {Button, View, StyleSheet, SafeAreaView} from 'react-native';
+import {Amplify} from 'aws-amplify';
+import {Authenticator, useAuthenticator} from '@aws-amplify/ui-react-native';
+
 import {NavigationContainer} from '@react-navigation/native';
 import MainNavigator from './src/navigation';
 
+Amplify.configure(outputs);
+
+const SignOutButton = () => {
+  const {signOut} = useAuthenticator();
+
+  return (
+    <View style={styles.signOutButton}>
+      <Button title="Sign Out" onPress={signOut} />
+    </View>
+  );
+};
+
 const App = () => {
   return (
-    <NavigationContainer>
-      <MainNavigator />
-    </NavigationContainer>
+    <Authenticator.Provider>
+      <Authenticator>
+        <SafeAreaView>
+          {/* <SignOutButton /> */}
+          <NavigationContainer>
+            <MainNavigator />
+          </NavigationContainer>
+        </SafeAreaView>
+      </Authenticator>
+    </Authenticator.Provider>
   );
 };
 
 export default App;
+const styles = StyleSheet.create({
+  signOutButton: {
+    alignSelf: 'flex-end',
+  },
+});
