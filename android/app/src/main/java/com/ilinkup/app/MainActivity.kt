d3a3a50528ba31fe16cb1e32e1fd458ae5941164
George-Kibe <georgekibewambui@gmail.com>
2024-06-10 10:45:02 +0300

    correcting bundle name

--- android/app/src/main/java/com/ilinkup/app/MainActivity.kt
+++ android/app/src/main/java/com/ilinkup/app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.ilinkup
+package com.iLinkup.app
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
--- android/app/src/main/java/com/ilinkup/app/MainApplication.kt
+++ android/app/src/main/java/com/ilinkup/app/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.ilinkup
+package com.iLinkup.app
 
 import android.app.Application
 import com.facebook.react.PackageList
