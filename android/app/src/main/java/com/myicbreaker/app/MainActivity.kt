189526eec80641167f6d1498b725c719f8d88d01
George-Kibe <georgekibewambui@gmail.com>
2024-08-04 00:36:05 +0300

    app icon on app list error

--- android/app/src/main/java/com/myicbreaker/app/MainActivity.kt
+++ android/app/src/main/java/com/myicbreaker/app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.myicbreaker.datingapp
+package com.myicbreaker.app
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
--- android/app/src/main/java/com/myicbreaker/app/MainApplication.kt
+++ android/app/src/main/java/com/myicbreaker/app/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.myicbreaker.datingapp
+package com.myicbreaker.app
 
 import android.app.Application
 import com.facebook.react.PackageList
