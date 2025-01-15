44ee6162f521c752da09c78c5d73bfa8cb141faf
George-Kibe <georgekibewambui@gmail.com>
2025-01-15 13:51:11 +0300

    profile image or passport

--- DigitalLicense/app/(tabs)/settings.tsx
+++ DigitalLicense/app/(tabs)/settings.tsx
@@ -1,4 +1,9 @@
+import SignPad from '@/components/SignPad';
 import React, { useState } from 'react';
+import CalendarPicker from "react-native-calendar-picker";
+import {Picker} from '@react-native-picker/picker';
+import * as ImagePicker from 'expo-image-picker';
+
 import {
   View,
   Text,
@@ -16,16 +21,51 @@ import {
 const ProfileEditScreen = () => {
   const [profileImage, setProfileImage] = useState<string | null>(null);
   const [name, setName] = useState('');
-  const [age, setAge] = useState('');
   const [address, setAddress] = useState('');
   const [dob, setDob] = useState('');
-  const [status, setStatus] = useState('');
-  const [className, setClassName] = useState('');
+  const [licenceNumber, setLicenceNumber] = useState('')
+  const [classType, setClassType] = useState('');
+  const [cardNumber,  setCardNumber] = useState('');
+  const [type, setType] = useState('');
+  const [expiry, setExpiry] = useState('');
 
-  const handleImageUpload = () => {
-    // Mock function for handling profile image update
-    alert('Open Image Picker');
+  const handleImageUpload = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ['images', 'videos'],
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.canceled) {
+      setProfileImage(result.assets[0].uri);
+    }
   };
+
+  console.log(
+    "Name: ", name,
+    "Address: ", address,
+    "DOB: ", dob,
+    "Licence Number: ", licenceNumber,
+    "Class Type: ", classType,
+    "Type: ", type,
+    "Expiry: ", expiry
+  )
+
+  const typeOptions = [
+    {"key":"(O)", "value": "Open"},
+    {"key":"(P)", "value": "P-Red"},
+    {"key":"(P)", "value": "P-Green"},
+    {"key":"(L)", "value": "Learner"}
+  ]
+  const classOptions = [ 
+    {"key":"(C)", "value": "Car"},
+    {"key":"(M)", "value": "Motorcycle"},
+    {"key":"(T)", "value": "Truck"},
+  ]
 
   const handleSave = () => {
     // Handle saving data
@@ -52,23 +92,65 @@ const ProfileEditScreen = () => {
         </SafeAreaView>
 
         {/* Name */}
-        <Text style={styles.label}>Name</Text>
+        <Text style={styles.label}>Full Name</Text>
         <TextInput
           style={styles.input}
           placeholder="Enter your name"
           value={name}
           onChangeText={setName}
         />
+        {/* Date of Birth */}
+        <Text style={styles.label}>Date of Birth</Text>
+        <CalendarPicker onDateChange={setDob} />
 
-        {/* Age */}
-        <Text style={styles.label}>Age</Text>
+        {/* Licence Number */}
+        <Text style={styles.label}>Licence Number</Text>
         <TextInput
           style={styles.input}
-          placeholder="Enter your age"
-          value={age}
-          onChangeText={setAge}
-          keyboardType="numeric"
+          placeholder="Enter your Licence Number"
+          value={licenceNumber}
+          onChangeText={setLicenceNumber}
         />
+
+        {/* Card Number */}
+        <Text style={styles.label}>Card Number</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Enter your Card Number"
+          value={cardNumber}
+          onChangeText={setCardNumber}
+        />
+
+        {/* Class Dropdown */}
+        <Text style={styles.label}>Class</Text>
+        <Picker
+          selectedValue={classType}
+          onValueChange={(itemValue, itemIndex) =>
+            setClassType(itemValue)
+          }>
+            {
+              classOptions.map( (option, index) => (
+                <Picker.Item 
+                  key={index} 
+                  label={option.key+" "+option.value} 
+                  value={option.key+" "+option.value} 
+                />
+              ))
+            }
+        </Picker>
+         {/* Type */}
+         <Text style={styles.label}>Type</Text>
+          <Picker    selectedValue={type} onValueChange={(itemValue, itemIndex) => setType(itemValue)}>
+              {
+                typeOptions.map( (option, index) => (
+                  <Picker.Item
+                    key={index}
+                    label={option.key+" "+option.value}
+                    value={option.key+" "+option.value}
+                  />
+                ))
+              }
+          </Picker>
 
         {/* Address */}
         <Text style={styles.label}>Address</Text>
@@ -79,35 +161,15 @@ const ProfileEditScreen = () => {
           onChangeText={setAddress}
         />
 
-        {/* Date of Birth */}
-        <Text style={styles.label}>Date of Birth</Text>
-        <TextInput
-          style={styles.input}
-          placeholder="Enter date of birth (e.g., DD/MM/YYYY)"
-          value={dob}
-          onChangeText={setDob}
-        />
-
-        {/* Status */}
-        <Text style={styles.label}>Status</Text>
-        <TextInput
-          style={styles.input}
-          placeholder="Enter your status"
-          value={status}
-          onChangeText={setStatus}
-        />
-
-        {/* Class */}
-        <Text style={styles.label}>Class</Text>
-        <TextInput
-          style={styles.input}
-          placeholder="Enter your class"
-          value={className}
-          onChangeText={setClassName}
-        />
+        {/* Expiry: Calendar */}
+        <Text style={styles.label}>Expiry</Text>
+        <CalendarPicker onDateChange={setExpiry} />
+        <Text style={styles.label}>Sign</Text>
+        <SignPad />
 
         {/* Save Button */}
         <Button title="Save Profile" onPress={handleSave} />
+        
       </ScrollView>
     </KeyboardAvoidingView>
   );
@@ -117,6 +179,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    marginBottom: 100
   },
   scrollContent: {
     padding: 16,
@@ -126,9 +189,9 @@ const styles = StyleSheet.create({
     marginBottom: 20,
   },
   image: {
-    width: 100,
-    height: 100,
-    borderRadius: 50,
+    width: 120,
+    height: 140,
+    borderRadius: 4,
   },
   placeholder: {
     width: 100,
@@ -143,7 +206,8 @@ const styles = StyleSheet.create({
   },
   label: {
     fontSize: 16,
-    marginBottom: 8,
+    marginBottom: 4,
+    fontWeight: '600',
     color: '#333',
   },
   input: {
--- DigitalLicense/declarations.d.ts
+++ DigitalLicense/declarations.d.ts
@@ -2,6 +2,8 @@ declare module '*.jpeg' {
   const value: any;
   export default value;
 }
+declare module 'react-native-signature-capture-view'
+
 declare module '*.png' {
   const value: any;
   export default value;
@@ -22,23 +24,19 @@ declare module '*.mp4' {
 
 type User = {
   _id?: string,
-  username: string,
-  nickname?: string,
-  email: string,
-  maritalStatus?: string,
-  sexualOrientation?: string,
-  clerkUserId: string,
-  first_name?: string,
-  name?: string,
-  last_name?: string,
-  age?: number,
-  minAge?: number,
-  maxAge?: number,
-  bio?: string,
-  greetingMessage?: string,
-  myAvatars?: string[],
-  children?: number,
-  livingWithMe?: string,
+  firstName?: string,
+  middleName?: string,
+  lastName?: string,
+  passportImage: string,
+  signatureImage: string,
+  fullName: string,
+  email?: string,
+  dateOfBirth: Date,
+  age: number,
+  address: string,
+  status?: string,
+  licenceNumber?: string,
+  cardNumber: string,
+  class?: string,
   gender?: string,
-  status?: string
 }
