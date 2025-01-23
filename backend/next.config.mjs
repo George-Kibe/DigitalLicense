1b66b597d1800baaccb03d4267d367b802c627f5
George-Kibe <georgekibewambui@gmail.com>
2025-01-23 18:17:59 +0300

    env bugs

--- backend/next.config.mjs
+++ backend/next.config.mjs
@@ -1,9 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: [
-      "lh3.googleusercontent.com",
-      "qddl-images-bucket.s3.amazonaws.com"
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com'
+      },
+      {
+        protocol: 'https',
+        hostname: 'qddl-images-bucket.s3.amazonaws.com'
+      },
     ]
   }
 };
--- backend/src/lib/uploadImageToS3.js
+++ backend/src/lib/uploadImageToS3.js
@@ -1,13 +1,13 @@
 import { S3Client } from "@aws-sdk/client-s3";
 import { Upload } from "@aws-sdk/lib-storage";
 
+const AWS_ACCESS_KEY_ID = process.env.NEXT_PUBLIC_AWS_ACCESS_KEY_ID;
+const MONGODB_URI = process.env.MONGODB_URI;
+const AWS_SECRET_ACCESS_KEY = process.env.NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY;
+const AWS_S3_REGION = process.env.NEXT_PUBLIC_AWS_S3_REGION;
+const S3_BUCKET_NAME = process.env.NEXT_PUBLIC_S3_BUCKET_NAME;
+
 async function uploadImageToS3(file, ext) {
-  const AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID;
-  const MONGODB_URI = process.env.MONGODB_URI;
-  const AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY;
-  const AWS_S3_REGION = process.env.AWS_S3_REGION;
-  const S3_BUCKET_NAME = process.env.S3_BUCKET_NAME;
-  
   console.log(
     "MONGODB_URI: ", MONGODB_URI,
     "AWS_ACCESS_KEY_ID: ", AWS_ACCESS_KEY_ID,
