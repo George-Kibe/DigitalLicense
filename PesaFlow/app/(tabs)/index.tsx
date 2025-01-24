a8fdacd18005b8d185b940a4cb529463fc54e1ca
George-Kibe <georgekibewambui@gmail.com>
2025-01-24 12:37:33 +0300

    sample application

---
+++ PesaFlow/app/(tabs)/index.tsx
@@ -0,0 +1,45 @@
+import { Image, StyleSheet, Platform, ScrollView, SafeAreaView } from 'react-native';
+
+import { HelloWave } from '@/components/HelloWave';
+import ParallaxScrollView from '@/components/ParallaxScrollView';
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
+
+export default function HomeScreen() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <ThemedText type="title">Welcome!</ThemedText>
+      <HelloWave />
+      <ThemedView style={styles.titleContainer}>
+        <ThemedText>
+          This is jut a test application
+        </ThemedText>
+      </ThemedView>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+  },
+  titleContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 8,
+  },
+  stepContainer: {
+    gap: 8,
+    marginBottom: 8,
+  },
+  reactLogo: {
+    height: 178,
+    width: 290,
+    bottom: 0,
+    left: 0,
+    position: 'absolute',
+  },
+});
