f17160a961992b0dd07f7e5ba13efb28adaa558e
George-Kibe <georgekibewambui@gmail.com>
2025-01-28 22:24:15 +0300

    nginx config for https

--- backend/src/app/user-functions/[code]/page.jsx
+++ backend/src/app/user-functions/[code]/page.jsx
@@ -24,8 +24,7 @@ import Link from 'next/link';
 import { generateCardNumber, generateLicenceNumber } from '@/lib/generateUserCode';
 import ConfirmDetailsAlert from '@/components/ConfirmDetailsAlert';
 
-// const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL
-const BACKEND_URL = "http://54.153.152.154:3000"
+const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL
 
 const UserFunctionsPage = () => {
   const params = useParams();
